bdc0c9dc2422839e24f9d7bca47c3e6e1054e946
chappiii <chappi787@gmail.com>
2023-01-02 20:10:00 +0300

    auth-status fix

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAllUsers, userSignup, userLogin } from "../controllers/user-controllers.js";
+import { getAllUsers, userSignup, userLogin, verifyUser } from "../controllers/user-controllers.js";
 import {validate, signupValidator, loginValidator} from "../utils/validators.js";
 import { verifyToken } from "../utils/token-manager.js";
 
@@ -8,6 +8,6 @@ const userRoutes = Router();
 userRoutes.get("/", getAllUsers);
 userRoutes.post("/signup", validate(signupValidator), userSignup);
 userRoutes.post("/login", validate(loginValidator), userLogin);
-userRoutes.get("/auth-status", verifyToken, userLogin);
+userRoutes.get("/auth-status", verifyToken, verifyUser);
 
 export default userRoutes; 
--- backend/src/utils/token-manager.ts
+++ backend/src/utils/token-manager.ts
@@ -12,19 +12,19 @@ export const createToken = (id: string, email:string, expiresIn:string) =>{
 
 export const verifyToken = async (req:Request, res:Response, next: NextFunction) =>{
     const token = req.signedCookies[`${COOKIE_NAME}`];
-  if (!token || token.trim() === "") {
-    return res.status(401).json({ message: "Token Not Received" });
-  }
-  return new Promise<void>((resolve, reject) => {
-    return jwt.verify(token, process.env.JWT_SECRET, (err, success) => {
-      if (err) {
-        reject(err.message);
-        return res.status(401).json({ message: "Token Expired" });
-      } else {
-        resolve();
-        res.locals.jwtData = success;
-        return next();
-      }
+    if (!token || token.trim() === "") {
+      return res.status(401).json({ message: "Token Not Received" });
+    }
+    return new Promise<void>((resolve, reject) => {
+      return jwt.verify(token, process.env.JWT_SECRET, (err, success) => {
+        if (err) {
+          reject(err.message);
+          return res.status(401).json({ message: "Token Expired" });
+        } else {
+            resolve();
+          res.locals.jwtData = success;
+          return next();
+        }
+      });
     });
-  });
-};
+}
