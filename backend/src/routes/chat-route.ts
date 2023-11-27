c8b44e2a93ffdbecabc800b47c846d2f0e99b5c7
chappiii <chappi787@gmail.com>
2023-11-27 05:53:12 +0300

    openai

--- backend/src/routes/chat-route.ts
+++ backend/src/routes/chat-route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { verifyToken } from "../utils/token-manager.js";
 import { chatCompletionValidator, validate } from "../utils/validators.js";
+import { generateChatCompletion } from "../controllers/chat-controller.js";
 
 // protected
 const chatRoutes = Router();
 
-chatRoutes.post("/new", validate(chatCompletionValidator), verifyToken)
+chatRoutes.post("/new", validate(chatCompletionValidator), verifyToken, generateChatCompletion)
 
 export default chatRoutes; 
